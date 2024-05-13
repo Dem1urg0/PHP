c148d7e128d1baacc50c23a32d9c21ee5d28cb0e
main <artem.graboveckii@gmail.com>
2024-05-13 19:25:04 +0300

    HW2.1 Объявить две целочисленные переменные $a и $b и задать им произвольные начальные значения. Затем написать скрипт, который работает по следующему принципу: Если $a и $b положительные, вывести их разность. Если $а и $b отрицательные, вывести их произведение. Если $а и $b разных знаков, вывести их сумму. Ноль можно считать положительным числом.

--- app/index.php
+++ app/index.php
@@ -1,18 +1,11 @@
 <?php
-$a = 'HELLO';
-$b = 'WORLD';
-$time = date('Y-m-d H:i:s');
-?>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title><?= $a ?></title>
-</head>
-<body>
-<h1><?= $b ?></h1>
-<h2><?= $time ?></h2>
-</body>
-</html>
+$a = rand(-15, 15);
+$b = rand(-15, 15);
+echo "$a and $b";
+if ($a >= 0 && $b >= 0) {
+    echo $a - $b;
+} else if ($a <= 0 && $b <= 0) {
+    echo $a * $b;
+} else {
+    echo $a + $b;
+}
